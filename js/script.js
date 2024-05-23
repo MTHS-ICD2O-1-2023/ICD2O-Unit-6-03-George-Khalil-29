6a0a485d37c0f0f751a91ee8b6f13ce1cf137193
George Khalil <159185251+George-Khalil-29@users.noreply.github.com>
2024-05-23 18:05:37 +0000

    save

--- js/script.js
+++ js/script.js
@@ -5,17 +5,29 @@
 // This file contains the JS functions for index.html
 
 "use strict"
-  
-async function getTemperature() {
+
+/**
+ * This function gets the Cat fact.
+ * The "async" is there beause it wil take time for the internet to return the value
+ */
+async function getCatFact() {
+  // the "try" id here because the internet may not be working
+  // it is like an "if ... else" statement"
   try {
     const resultJSON = await fetch("https://cat-fact.herokuapp.com/facts")
     const jsonData = await resultJSON.json()
     //console.log(jsonData)
-    const kelvinToCelsius = (kelvin) => kelvin - 273.15;
-    document.getElementById('result').innerHTML = kelvinToCelsius
-    const firstDataSet = jsonData[0]
+
+    // NOTE: there are 5 (0 to 4) cat facts
+    const randomFactNumber = Math.floor(Math.random() * 5)
+    const firstDataSet = jsonData[randomFactNumber]
     const textDataSet = firstDataSet.text
     console.log(textDataSet)
+
+    // output
+    // add 1 to random number, since users would see Fact #0 as odd!
+    document.getElementById("cat-fact-number").innerHTML = "<p>Cat Fact #" + (randomFactNumber + 1) + "</p>"
+    document.getElementById("cat-fact").innerHTML = "<p>" + textDataSet + "</p>"
   } catch (error) {
     console.error(error)
   }
